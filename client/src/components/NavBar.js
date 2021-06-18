fdda7848d334fcedc37ba104c131d71112603a6f
Kp <kp_1@outlook.com>
2021-06-18 15:17:55 +0400

    Updates

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import './style.css';
+import './nav.css';
 
 export default function NavBar() {
   return (
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="/" >
-            Google Books Search
-          </a>
-          <br></br>
-          <a className="navbar-brand" href="/savedbooks">
-            Saved Books List
-          </a>
-        </nav>
-      );
-    }
+    <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+      <a className='navbar-brand' href='/'>
+        Google Books Search
+      </a>
+      <br></br>
+      <a className='navbar-brand' href='/savedbooks'>
+        Saved Books List
+      </a>
+    </nav>
+  );
+}
