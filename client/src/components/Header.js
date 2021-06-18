52caae8bc91aa97222eeb7c2ce72ac95209e668e
Kp <kp_1@outlook.com>
2021-06-18 12:22:12 +0400

    populate Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div className='jumbotron text-center'>
+        <div className='container text-dark'>
+          <h1>Google Books Search </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
