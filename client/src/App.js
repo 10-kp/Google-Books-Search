3cb307b0b2030dd05be14cd8fface492020c258e
Kp <kp_1@outlook.com>
2021-06-19 07:49:17 +0400

    Updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Header from './components/Header';
-import Wrapper from './components/Wrapper';
-
-// import NoMatch from "./pages/NoMatch";
-// import Search from "./pages/Search";
-// import Saved from "./pages/Saved";
+import SavedBooks from './pages/SavedBooks';
+import Books from './pages/Books';
+import Nav from './components/NavBar';
 
 import './App.css';
 
