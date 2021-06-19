86041c98b6e16a8c1b0a99bff088d6f575cbb54b
Kp <kp_1@outlook.com>
2021-06-19 16:07:17 +0400

    Button Folder in components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-export default function Header() {
+function Jumbotron({ children }) {
   return (
-    <div>
-      <div className='jumbotron text-center'>
-        <div className='container text-dark'>
-          <h1>Google Books Search </h1>
-        </div>
-      </div>
+    <div
+      style={{
+        height: 150,
+        clear: 'both',
+        paddingTop: 50,
+        textAlign: 'center',
+      }}
+      className='jumbotron'
+    >
+      {children}
     </div>
   );
 }
+
+export default Jumbotron;
